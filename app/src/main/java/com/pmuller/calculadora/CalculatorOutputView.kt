e8207e3eab1a3b9de88f0939601ad426ede1293b
Pablo <73319335+pmuller2010@users.noreply.github.com>
2021-03-24 21:10:28 -0300

    Create CalculatorOutputView.kt

---
+++ app/src/main/java/com/pmuller/calculadora/CalculatorOutputView.kt
@@ -0,0 +1,10 @@
+package com.pmuller.calculadora
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CalculatorOutputView (context: Context, attributeSet: AttributeSet?): FrameLayout(context,attributeSet){
+
+    
+}
